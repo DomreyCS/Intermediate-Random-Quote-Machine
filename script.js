64e65ba570f177b8045745ae6b7a4a25a70e63de
Dara Hoy <darahoy@gmail.com>
2018-05-17 16:32:10 +0000

    Fix API cache issue, new quote request will now update without developer tools open

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
-//Create event listener
-document.getElementById('getQuote').addEventListener('click', getQuote);
+// Create an event listener for button
+document.getElementById('getQuote').addEventListener('click', requestQuote);
 
-function getQuote() {
-    //Create the XHR object
-    let xhr = new XMLHttpRequest();
-    // .open(method, url/file, async)
-    xhr.open('GET', 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1', true)
-    // Handles API response
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-            var data = JSON.parse(this.responseText); // JSON.parse translates the object
-            document.getElementById('quote').innerHTML = `${data[0].content}`;
-            document.getElementById('author').innerHTML = `&mdash; ${data[0].title}`;
-        }
-    }
-    xhr.onerror = function() {
-        console.log('Request Error...')
-    }
-    //Sends request
-    xhr.send();
+function requestQuote(){
+	// Create the XHR object
+	let xhr, url;
+	xhr = new XMLHttpRequest();
+	// Assign URL variable
+	url = 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand';
+	// Handles API response
+	xhr.onload = updateQuote;
+	xhr.open('GET', url + ((/\?/).test(url) ? "&" : "?") + (new Date()).getTime(), true); //prevents caching API response by adding new Date object to params
+	xhr.send(null);
+}
+
+function updateQuote() {
+    if (this.readyState && this.status === 200) {
+		let quote = JSON.parse(this.responseText).shift(); // Converts text to JSON and assigns first object from array
+		console.log(quote);
+        document.getElementById('quote').innerHTML = `${quote.content}`; // Update quote
+	    document.getElementById('author').innerHTML = `&mdash; ${quote.title}`; // Update author
+	} else {
+	    return false;
+	}
+	
 }
